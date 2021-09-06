2b29986e45535749eee0ffca3f70c6e2a07f0164
Holla.Yamini <holla.yamini@bp.com>
2021-09-06 19:58:17 +0100

    commit

--- random.tf
+++ random.tf
@@ -2,9 +2,6 @@ resource "random_pet" "petname" {
     length = "5"
     separator = "-"
 }
-resource "aws_s3_bucket" "sample" {
-  bucket = "random_pet.petname.id"
-  acl = "public-read"
-    }
+
 
 
