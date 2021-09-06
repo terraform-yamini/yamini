bdca712d5c3a87f7c088e9345859c822027dcc13
Holla.Yamini <holla.yamini@bp.com>
2021-09-06 20:20:22 +0100

    tags

--- c2-ec2.tf
+++ c2-ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my-ec2-vm" {
     ami = "ami-047a51fa27710816e"
     instance_type = "t2.micro"
     availability_zone = "us-east-1a"
-    tags {
+    tags = {
         Name = "web"
     }
 }
