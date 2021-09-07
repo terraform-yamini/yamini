effc4cacf8a5fb0ce75c7758a2f88dfe184160fc
Holla.Yamini <holla.yamini@bp.com>
2021-09-07 15:17:39 +0100

    commit

--- sg.tf
+++ sg.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "vpc_ssh" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
         description = "Allow all Ip and Ports around"
@@ -24,14 +24,14 @@ resource "aws_security_group" "vpc_web" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         description = "Allow port 443"
         from_port = 443
         to_port = 443
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
         description = "Allow all Ip and Ports around"
