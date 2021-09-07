050c0a837bc132b884216eb38838c77961ea3499
Holla.Yamini <holla.yamini@bp.com>
2021-09-07 16:56:35 +0100

    vasrs

--- variables.tf
+++ variables.tf
@@ -4,11 +4,7 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-variable "ec2_instance_type" {
-  description = "Region in which AWS resources to be created"
-  type        = string
-  default     = "t2.micro"
-}
+
 variable "ec2_ami_id" {
   description = "AMI ID"
   type        = string
@@ -18,8 +14,9 @@ variable "ec2_ami_id" {
 variable "ec2_instance_count" {
   description = "EC2 Instance Count"
   type        = number
-  default     = 1
+  default     = 2
 }
+
 variable "ec2_instance_type" {
   description = "EC2 Instance Type"
   type = string
