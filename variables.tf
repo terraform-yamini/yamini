ca70dbf4a90b0bc77a8cedeaff19d4db51bf3c37
Holla.Yamini <holla.yamini@bp.com>
2021-09-07 18:03:11 +0100

    commit

--- variables.tf
+++ variables.tf
@@ -20,12 +20,13 @@ variable "ec2_instance_count" {
   type        = number
   default     = 2
 }
-
+/*
 variable "ec2_instance_type" {
   description = "EC2 Instance Type"
   type = string
   default = ["t3.micro", "t3.small", "t3.large"]
 }
+*/
 variable "ec2_instance_tags" {
   description = "EC2 Instance Tags"
   type = map(string)
