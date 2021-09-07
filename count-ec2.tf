9f3e7b98e7bf702bf9be3a32d59604294d12550a
Holla.Yamini <holla.yamini@bp.com>
2021-09-07 11:34:39 +0100

    tags

--- count-ec2.tf
+++ count-ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
     ami = "ami-047a51fa27710816e"
     instance_type = "t2.micro"
     count = "5"
-    tags {
-        Name = "web-${count.index}"
+    tags = {
+        "Name" = "web-${count.index}"
     }
 }
