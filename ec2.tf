e26c1631e61c010e657898185978aae90dc00bb2
Holla.Yamini <holla.yamini@bp.com>
2021-09-07 18:08:04 +0100

    commit

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,10 @@
-# Resource-8: Create EC2 Instance
+# Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
   ami                    = var.ec2_ami_id
   instance_type          = var.ec2_instance_type
   key_name               = "terraform-key"
-  subnet_id              = aws_subnet.vpc-dev-public-subnet-1.id
-  vpc_security_group_ids = [aws_security_group.dev-vpc-sg.id]
-  count = var.ec2_instance_count
-  user_data = file("apache-install.sh")
-  /*user_data = <<-EOF
+  count                  = var.ec2_instance_count
+  user_data              = <<-EOF
     #!/bin/bash
     sudo yum update -y
     sudo yum install httpd -y
@@ -15,9 +12,8 @@ resource "aws_instance" "my-ec2-vm" {
     sudo systemctl start httpd
     echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
     EOF
-  */
-#  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
     "Name" = "myec2vm"
-  }    
+  }
 }
--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "ec2_ami_id" {
   description = "AMI ID"
   type        = string
   default     = "ami-0915bcb5fa77e4892" # Amazon2 Linux AMI ID
-    validation {
+  validation {
     condition = length(var.ec2_ami_id) > 4 && substr(var.ec2_ami_id, 0, 4) == "ami-"
     error_message = "The ec2_ami_id value must be a valid AMI id, starting with \"ami-\"."
   }
@@ -18,30 +18,11 @@ variable "ec2_ami_id" {
 variable "ec2_instance_count" {
   description = "EC2 Instance Count"
   type        = number
-  default     = 2
+  default     = 1
 }
-/*
+
 variable "ec2_instance_type" {
   description = "EC2 Instance Type"
   type = string
-  default = ["t3.micro", "t3.small", "t3.large"]
-}
-*/
-variable "ec2_instance_tags" {
-  description = "EC2 Instance Tags"
-  type = map(string)
-  default = {
-    "Name" = "ec2-web"
-    "Tier" = "Web"
-  }
-}
-
-variable "ec2_instance_type_map" {
-  description = "EC2 Instance Type"
-  type = map(string)
-  default = {
-    "small-apps" = "t3.micro"
-    "medium-apps" = "t3.medium" 
-    "big-apps" = "t3.large"    
-  }
+  default = "t3.micro"
 }
