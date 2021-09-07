bd63b5048eb564e137e3eec2c756368d0a3a996a
Holla.Yamini <holla.yamini@bp.com>
2021-09-07 18:46:27 +0100

    ec2changes

--- ec2.tf
+++ ec2.tf
@@ -1,19 +1,20 @@
-# Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
-  ami                    = var.ec2_ami_id
-  instance_type          = var.ec2_instance_type
-  key_name               = "terraform-key"
-  count                  = 1
-  user_data              = <<-EOF
+  ami           = var.ec2_ami_id 
+  instance_type = var.ec2_instance_type
+  key_name      = "terraform-key"
+	user_data = file("apache-install.sh")  
+  /*
+    user_data     = <<-EOF
     #!/bin/bash
     sudo yum update -y
     sudo yum install httpd -y
     sudo systemctl enable httpd
     sudo systemctl start httpd
-    echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    echo "<html><body><div>Welcome to StackSimplify ! AWS Infra created using Terraform</div></body></html>" > /var/www/html/index.html
     EOF
+  */
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
-    "Name" = "myec2vm"
+    "Name" = "web"
   }
 }
