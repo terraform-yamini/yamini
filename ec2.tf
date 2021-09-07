b452dab8cd3e7599f7a41cc47f95bb3261439ab3
Holla.Yamini <holla.yamini@bp.com>
2021-09-07 11:08:23 +0100

    commitmessage

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,20 @@
-
-resource "aws_instance" "ec2-linux-demo" {
-    ami = "ami-087c17d1fe0178315"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.vpc-dev-public-subnet-1.id
-    key_name = "terraform-key"
-    user_data = << - EOF
-    #! /bin/bash
-    sudo yum update -y 
-    sudo yum install -y httpd
-    sudo service httpd start
+# Resource-8: Create EC2 Instance
+resource "aws_instance" "my-ec2-vm" {
+  ami                    = "ami-047a51fa27710816e"
+  instance_type          = "t2.micro"
+  key_name               = "terraform-key"
+  subnet_id              = aws_subnet.vpc-dev-public-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.dev-vpc-sg.id]
+  #user_data = file("apache-install.sh")
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
     sudo systemctl enable httpd
-    echo "<h1> welcome to stack simplify ! aws infra created using terraform in us-east-1</h1>" > /var/www/html/index.html
+    sudo systemctl start httpd
+    echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
     EOF
-    vpc_security_group_ids = [aws_security_group.dev-vpc-sg.id]
+  tags = {
+    "Name" = "myec2vm"
+  }    
 }
