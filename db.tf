c8c683dafe8843008c1b71d11c8c2e45ad30e4a8
Holla.Yamini <holla.yamini@bp.com>
2021-09-07 16:06:10 +0100

    db

--- db.tf
+++ db.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "db1" {
     engine = "mysql"
     instance_class = "db.t2.micro"
     name = "mydb1"
-    username = var.db_username 
-    password = var.db_password
+    username = "admin"
+    password = "insecurepassword"
     skip_final_snapshot = true
 }
