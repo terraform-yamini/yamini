f86c7c0c68dd9385afaea64698aa5a16da7f5822
Holla.Yamini <holla.yamini@bp.com>
2021-09-08 07:56:55 +0100

    commitdata

--- output.tf
+++ output.tf
@@ -10,12 +10,13 @@ output "ec2_instance_privateip" {
   description = "EC2 Instance Private IP"
   value = aws_instance.my-ec2-vm.private_ip 
 }
+/*
 # Argument Reference: Security Groups associated to EC2 Instance
 output "ec2_security_groups" {
   description = "List Security Groups associated with EC2 Instance"
   value = aws_instance.my-ec2-vm.security_groups
 }
-
+*/
 # Attribute Reference - Create Public DNS URL with http:// appended
 output "ec2_publicdns" {
   description = "Public DNS URL of an EC2 Instance"
--- variables.tf
+++ variables.tf
@@ -2,10 +2,12 @@ variable "aws_region" {
     type =  string 
     default = "us-east-1"
 }
+/*
 variable "ec2_ami_id" {
     type = string 
     default = "ami-0915bcb5fa77e4892"
 }
+*/
 variable "ec2_instance_type" {
     type = string 
     default = "t3.micro"
